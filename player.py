db666905d9a4c3d6c9a8a5449e8cb14e7309e375
Bhushan <bhushan1810patil@gmail.com>
2020-10-20 12:23:08 +0530

    add player file

---
+++ player.py
@@ -0,0 +1,21 @@
+class Player:
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.w = 20
+        self.h = 40
+        self.vx = 0
+        self.vy = 0
+        self.ay = 0
+        
+    
+    def draw(self, win):
+        pass
+
+    def move(self):
+        pass
+
+    def update_velocity(self, vel):
+        pass
+
