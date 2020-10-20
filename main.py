7c5c056708a7dc0568ef4ff148dd12d432574be2
Bhushan <bhushan1810patil@gmail.com>
2020-10-20 14:30:10 +0530

    blind commit

--- main.py
+++ main.py
@@ -13,39 +13,60 @@ class Game:
         self.height = HEIGHT
         self.fps = FPS
         self.win = None
+        self.gameWin = None
         self.clock = None
-        self.font = None
+        self.titleFont = None
+        self.textFont = None
         self.gameWidth = GW_WIDTH
         self.gameHeight = GW_HEIGHT
         self.xoff = XOFF
         self.yoff = YOFF
+        self.gameStart = False
 
     
     def game_init(self):
-
+        # init
         pygame.init()
         pygame.font.init()
 
+        # windows init
         self.win = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption("Whirly Bird")
         self.win.fill(colors.MID_BLACK)
         
-        self.gameWidth = self.win.subsurface((self.xoff,\
+        self.gameWin = self.win.subsurface((self.xoff,\
             self.yoff, self.gameWidth, self.gameHeight))
 
-        self.font = pygame.font.SysFont('comicsansms', FONT_SIZE)
+        # fonts init
+        self.titleFont = pygame.font.SysFont('comicsansms', FONT_SIZE)
+        self.textFont = pygame.font.SysFont('impact', int(FONT_SIZE // 1.5))
         
-        title = self.font.render('Whirly Bird Developer', 1, colors.GOLD)
+        # Block for Title 
+        title = self.titleFont.render('Whirly Bird Developer', 1, colors.GOLD)
         fontX = (self.win.get_width() - title.get_width()) // 2
         fontY = (YOFF - title.get_height()) // 2
 
+        # Background
         self.win.blit(title, (fontX, fontY))
-        
+
+        # Clock init
         self.clock = pygame.time.Clock()
 
     def draw(self):
-        self.gameWidth.fill(colors.MINT_CREAM)
+        self.gameWin.fill(colors.MINT_CREAM)
+
+        if not self.gameStart:
+            text = self.textFont.render('Press Enter to start the game', 1, colors.SADDLE_BROWN)
+            textX = (self.gameWin.get_width() - text.get_width()) // 2
+            textY = self.yoff + (self.gameWin.get_width() - text.get_height()) // 2
+        
+            self.gameWin.blit(text, (textX, textY))
+
         pygame.display.update()
+
+    def new_game(self):
+        pass
+
 
     def run(self):
         self.game_init()
@@ -59,6 +80,13 @@ class Game:
 
                 if event.type == pygame.QUIT:
                     run = False
+
+                if event.type == pygame.KEYDOWN:
+                    pressed = pygame.key.get_pressed()
+
+                    if pressed[pygame.K_RETURN]:
+                        self.gameStart = True
+                        self.new_game()
 
             self.draw()
 
--- player.py
+++ player.py
@@ -1,14 +1,14 @@
 class Player:
 
-    def __init__(self):
-        self.x = 0
-        self.y = 0
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
         self.w = 20
         self.h = 40
         self.vx = 0
         self.vy = 0
         self.ay = 0
-        
+
     
     def draw(self, win):
         pass
