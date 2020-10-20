4c2c68fb2fc0620b09b652033294ccb772e276d2
Bhushan <bhushan1810patil@gmail.com>
2020-10-20 13:05:52 +0530

    code to change the background color

--- images.py
+++ images.py
@@ -1,7 +1,13 @@
 import pygame
 import os
+from functools import reduce
+from colors import MINT_CREAM
 
 asset_path = os.path.join(os.getcwd(), 'assets')
+# paste_path = os.path.join(os.getcwd(), 'saved')
+
+# if not os.path.exists(paste_path):
+#     os.mkdir(paste_path)
 
 # Types:
 # man, capman, step_f, step_h, step_i
@@ -16,5 +22,19 @@ asset_path = os.path.join(os.getcwd(), 'assets')
 # step_c - 40:20 - (1) - crunch - 40:30
 # step_s - 35:25 - (1) - up - 35:15 and 35:30
 # throne - 40:30
-img_dir = dict()
-img = pygame.image.load(os.path.join(asset_path, 'cap1.jpg'))
+# img_dir = dict()
+# img = pygame.image.load(os.path.join(asset_path, 'cap1.jpg'))
+
+
+# for file in os.listdir(asset_path):
+#     print(file)
+#     if os.path.splitext(file)[1] == '.jpg':
+#         img = pygame.image.load(os.path.join(asset_path, file))
+#         for x in range(img.get_width()):
+#             for y in range(img.get_height()):
+#                 T = img.get_at((x, y))
+#                 L = [i > 240 for i in T]
+#                 if reduce(lambda x, y : x and y, L):
+#                     img.set_at((x, y), MINT_CREAM)
+
+#     pygame.image.save(img, os.path.join(paste_path, file))
