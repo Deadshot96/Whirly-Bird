48739461ea463a3452d169e27f6731b36347e663
Bhushan <bhushan1810patil@gmail.com>
2020-10-20 13:23:23 +0530

    create gamewin and font and tite

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ class Game:
         self.win = None
         self.clock = None
         self.font = None
+        self.gameWidth = GW_WIDTH
+        self.gameHeight = GW_HEIGHT
+        self.xoff = XOFF
+        self.yoff = YOFF
+
     
     def game_init(self):
 
@@ -23,23 +28,39 @@ class Game:
 
         self.win = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption("Whirly Bird")
+        self.win.fill(colors.MID_BLACK)
+        
+        self.gameWidth = self.win.subsurface((self.xoff,\
+            self.yoff, self.gameWidth, self.gameHeight))
 
-        self.win.fill(colors.MINT_CREAM)
+        self.font = pygame.font.SysFont('comicsansms', FONT_SIZE)
+        
+        title = self.font.render('Whirly Bird Developer', 1, colors.GOLD)
+        fontX = (self.win.get_width() - title.get_width()) // 2
+        fontY = (YOFF - title.get_height()) // 2
 
+        self.win.blit(title, (fontX, fontY))
+        
         self.clock = pygame.time.Clock()
+
+    def draw(self):
+        self.gameWidth.fill(colors.MINT_CREAM)
+        pygame.display.update()
 
     def run(self):
         self.game_init()
+        self.draw()
 
         run = True
         while run:
             self.clock.tick(self.fps)
-            pygame.display.update()
 
             for event in pygame.event.get():
 
                 if event.type == pygame.QUIT:
                     run = False
+
+            self.draw()
 
         pygame.font.quit()
         pygame.quit()
